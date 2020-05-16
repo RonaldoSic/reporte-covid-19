b1edba96ceb37068f5eb025194bb1b200a2fb90f
Carlos Vásquez <carls10vasquez@gmail.com>
2020-05-16 10:12:33 -0600

    deploit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import { guateData } from './Data/guateData'
 import { byCountry, searchCountry } from './Data/countryData'
 import { globalData } from './Data/dataGlobal';
 import {titlesAllSections} from './Data/titlesAndContext';
-import Footer from './components/footer'
 export default class App extends Component {
   constructor(props){
     super(props);
@@ -107,7 +106,19 @@ export default class App extends Component {
           title={tituloInsert}
           description={parrafInsert}
           />
-          <Footer />
+          <footer>
+              <div className="col-container">
+                  <div className="noticias-nacionales nts">
+                      <h4>Noticias</h4>
+                      <div className="enlaces">
+                          <a href="https://www.mspas.gob.gt/index.php/noticias/covid-19/coronavirus-2019-ncov">Corona virus en Guatemala</a>
+                          <a href="https://www.mspas.gob.gt/index.php/noticias/covid-19/casos">Casos en Guatemala</a>
+                          <a href="https://www.bbc.com/mundo/noticias-51705060">Mapa de infectados</a>
+                      </div>
+                  </div>
+              </div>
+              <div className="escod">By ESCod</div>
+          </footer>
         </>
     )
   }
