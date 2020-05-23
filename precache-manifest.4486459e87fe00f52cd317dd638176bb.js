347042c06215b9ced89bcd2c2975feee8d028b0a
Carlos Vásquez <carls10vasquez@gmail.com>
2020-05-23 09:36:31 -0600

    Updates

---
+++ precache-manifest.4486459e87fe00f52cd317dd638176bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "648b6c6218adc01bac82231616dac137",
+    "url": "/reporte-covid-19/index.html"
+  },
+  {
+    "revision": "03fe012a918538bed8d0",
+    "url": "/reporte-covid-19/static/css/main.c8977188.chunk.css"
+  },
+  {
+    "revision": "cd1a0c6a331f8e85eafd",
+    "url": "/reporte-covid-19/static/js/2.be2ab774.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/reporte-covid-19/static/js/2.be2ab774.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03fe012a918538bed8d0",
+    "url": "/reporte-covid-19/static/js/main.6489aeeb.chunk.js"
+  },
+  {
+    "revision": "bb305a75e95627d4bc2f",
+    "url": "/reporte-covid-19/static/js/runtime-main.010b468b.js"
+  },
+  {
+    "revision": "947894b3f9bdbac3b1980a3588c481fd",
+    "url": "/reporte-covid-19/static/media/covid-19.947894b3.jpg"
+  }
+]);
